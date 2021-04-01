87d2d1d13467603ddb886105a2ca0785bffae8e0
Cory Bass <cory.bass.07@gmail.com>
2021-04-01 17:22:38 -0500

    demo of float type variables

--- DataTypesAndVariables/Program.cs
+++ DataTypesAndVariables/Program.cs
@@ -14,8 +14,13 @@ namespace DataTypesAndVariables
             double d2 = 1.337;
             double sumD = d1 + d2;
 
+            float f1 = 3.5f;
+            float f2 = 1.337f;
+            float sumF = f1 + f2;
+
             Console.WriteLine("The sum of " + num1  + " and " + num2 + " is " + sum);
             Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
+            Console.WriteLine("The sum of " + f1 + " and " + f2 + " is " + sumF);
             Console.Read();
         }
     }

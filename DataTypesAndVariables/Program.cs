6b80212dc1f5bec1f10d99cf7abe4a04c296c8e6
Cory Bass <cory.bass.07@gmail.com>
2021-04-01 17:16:21 -0500

    setting double variables and printing it with concatination

--- DataTypesAndVariables/Program.cs
+++ DataTypesAndVariables/Program.cs
@@ -10,7 +10,12 @@ namespace DataTypesAndVariables
             int num2 = 69;
             int sum = num1 + num2;
 
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+
             Console.WriteLine("The sum of " + num1  + " and " + num2 + " is " + sum);
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
             Console.Read();
         }
     }

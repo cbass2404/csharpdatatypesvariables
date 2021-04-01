49de65ace1137cffdf132c4895ade50ccaccef2e
Cory Bass <cory.bass.07@gmail.com>
2021-04-01 17:14:28 -0500

    setting int variables and printing it with concatination

--- DataTypesAndVariables/Program.cs
+++ DataTypesAndVariables/Program.cs
@@ -7,8 +7,10 @@ namespace DataTypesAndVariables
         static void Main(string[] args)
         {
             int num1 = 13;
+            int num2 = 69;
+            int sum = num1 + num2;
 
-            Console.WriteLine(num1);
+            Console.WriteLine("The sum of " + num1  + " and " + num2 + " is " + sum);
             Console.Read();
         }
     }
